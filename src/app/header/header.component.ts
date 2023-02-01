c1f326fe7fe692c412ed3df08b03ddc5457d4bea
Jabi <michal.jabi@gmail.com>
2023-02-01 12:15:44 +0100

    explain(@Input): show me the "props" in Angular.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   template: `
-    <p>
-      header works!
-    </p>
+    <header>
+      <h1> {{title}} </h1>
+    </header>
   `,
   styles: [
   ]
 })
 export class HeaderComponent {
+    @Input() title = 'Auction Portal'
+    // będzie też @Output() do komunikacji w 2 stronę.
 
+    // Możemy dowolną rzecz z logiki z mienić w czasie, - zaktualizuje się na widoku.
+    // constructor() {
+    //   setTimeout(() => {
+    //      this.title += '!'
+    //   }, 3000)
+    // }
 }
