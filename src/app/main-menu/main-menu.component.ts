dc5b4e8aea0f13426c34c85d96c6c8e71dc2925f
Jabi <michal.jabi@gmail.com>
2023-02-01 14:15:00 +0100

    explain(main-menu.component): ngClass directive and computed properties.

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-main-menu',
   template: `
-    <nav class="navbar navbar-expand-lg navbar-light bg-light px-3 mb-3">
-      <button class="navbar-toggler" type="button" (click)="handleTogglerClick()">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light px-3 mb-3" [title]="computedTitle">
+      <button class="navbar-toggler" type="button" (click)="handleTogglerClick()" [title]="80 + 20">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" [class.show]="isMenuShown">
+     <!-- <div class="collapse navbar-collapse" [class.show]="isMenuShown"> -->
+      <div class="collapse navbar-collapse" [ngClass]="{'show' : isMenuShown }">
         <ul class="navbar-nav">
           <li class="nav-item active">
             <a class="nav-link" href="auctions">Aukcje</a>
@@ -31,4 +32,9 @@ export class MainMenuComponent {
   handleTogglerClick() {
     this.isMenuShown = !this.isMenuShown;
   }
+
+  get computedTitle(): string {
+    return `This menu is ${ this.isMenuShown ? '' : 'NOT '}shown`
+  }
+
 }
