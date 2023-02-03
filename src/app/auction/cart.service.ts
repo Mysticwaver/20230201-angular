a0bcafe6719ede4295119c75e9ec20357cd2d6b4
Jabi <michal.jabi@gmail.com>
2023-02-03 10:41:36 +0100

    refactor(cart.service): make it Observable hold state with BehaviorSubject

--- src/app/auction/cart.service.ts
+++ src/app/auction/cart.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
+
 import { AuctionItem } from './auction-item'
 import { CartItem } from './cart-item'
 
@@ -7,6 +10,42 @@ import { CartItem } from './cart-item'
 @Injectable({
   providedIn: 'root'
 })
+export class CartService {
+  private item$ = new BehaviorSubject<CartItem[]>([])
+
+  addAuction(auction: AuctionItem) {
+    const items: CartItem[] = this.item$.getValue();
+    const item = items.find(i => i.id === auction.id)
+    if(item) {
+      item.amount++
+    } else {
+      items.push({
+        id: auction.id,
+        amount: 1,
+        auction
+      })
+    }
+    // MUST !!!! Koniecznie rozgłaszamy wszystkim subskrybentom zmiany!!!
+    this.item$.next(items);
+  }
+  getAll(): Observable<CartItem[]> {
+    return this.item$.asObservable()
+  }
+  getCount(): Observable<number> {
+    // ([])----------([A])-
+    //   |             |
+    //   map(....)     |
+    //    \             \
+    //     0             1
+    return this.item$.pipe(map((items) => items.reduce((amount, item) => amount + item.amount, 0)))
+  }
+  getValue(): Observable<number> {
+    return this.item$.pipe(map((items) => items.reduce((value, item) => value + item.amount * item.auction.price, 0)))
+  }
+}
+
+/*
+
 export class CartService {
   private items: CartItem[] = []
 
@@ -32,3 +71,5 @@ export class CartService {
     return this.items.reduce((value, item) => value + item.amount * item.auction.price, 0)
   }
 }
+
+*/
