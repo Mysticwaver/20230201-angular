51246f009428c12c70a152cfc5503f9f1dcc2038
Jabi <michal.jabi@gmail.com>
2023-02-03 12:36:12 +0100

    feat(AdviceDetailsComponent): use ActivatedRoute to read params

--- src/app/advice/advices-page/advice-details.component.ts
+++ src/app/advice/advices-page/advice-details.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   template: `
     <p>
-      advice-details works!
+      Wybrano id do załadowania {{ myAdviceId }}
     </p>
   `,
   styles: []
 })
 export class AdviceDetailsComponent {
 
+  myAdviceId = 0
+
+  constructor(private activatedRoute: ActivatedRoute) {
+    // Niepoprawnie bo mamy tylko 1 wartość
+    // this.myAdviceId = Number(activatedRoute.snapshot.params['adviceId']);
+
+    activatedRoute.params.subscribe((params) => {
+      this.myAdviceId = Number(params['adviceId']);
+    })
+  }
 }
